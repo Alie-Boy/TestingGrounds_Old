e4b47e5b193115c32d9298d8781913c5b98d603c
CyberBlast <ashishdantesparda@gmail.com>
2019-04-07 14:55:43 +0530

    TG024: Simplifying our project

--- Source/TG_FPS_Old/TG_FPS_OldHUD.cpp
+++ Source/TG_FPS_Old/TG_FPS_OldHUD.cpp
@@ -10,7 +10,7 @@
 ATG_FPS_OldHUD::ATG_FPS_OldHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
